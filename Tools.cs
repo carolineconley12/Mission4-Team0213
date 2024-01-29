ef40ba921d5eeeff85dfbc32f5735f21c8927068
carolineconley12 <cconley6@byu.edu>
2024-01-29 11:37:09 -0700

    new class file

---
+++ Tools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission4_Team0213
+{
+    internal class Tools
+    {
+        public void PrintBoard(char[] board)
+        {
+            Console.WriteLine($" {board[0]} | {board[1]} | {board[2]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} ");
+        }
+
+
+    }
+}
