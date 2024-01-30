3060b1d400bc8d9ed0a5c516a692b36df30e79f5
tiffanyzang <tz252@byu.edu>
2024-01-29 17:27:53 -0700

    finished the validate method

--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@
 using System.Security.Cryptography.X509Certificates;
 using Mission4_Team0213;
 using Tools.cs;
-
-// please please
 class Game
 {
     static void Main()
@@ -13,7 +11,7 @@ class Game
         // Initialize the game board
         int[] board = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-        int[] choices = { };
+        char[] choices = { };
 
         int turn = 1;
 
@@ -25,33 +23,46 @@ class Game
         {
             for (int i = 0; i < board.Length; i++)
             {
+
                 if (turn % 2 != 0)
                 {
                     Console.WriteLine("Player 1, enter your position choice (1-9):");
-                    char guess = Console.ReadLine();
+                    char guess = char.Parse(Console.ReadLine());
+
+                    ValidGuess(guess, choices);
 
                     choices[i] = char.Parse(Console.ReadLine());
 
+
+                    turn++;
+
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Player 2, enter your position choice (1-9):");
+                    string guess = Console.ReadLine();
+
+                    choices[i] = char.Parse(Console.ReadLine());
+                    turn++;
                 }
             }
 
         } while (play == true);
-
-
-
-
-
     }
 
-    public bool ValidGuess(string guess, string choices)
+    public bool ValidGuess(char guess, char[] choices)
     {
         bool result = true;
-        if (!Char.IsNumber(guess[0]))
+        int guessNumber = int.Parse(guess.ToString());
+
+        if (!Char.IsNumber(guess))
         {
             Console.WriteLine("Input must be a number. Try again");
             result = false;
         }
-        else if ((int.Parse(guess) < 1) || int.Parse(guess) > 9)
+        else if ((guessNumber) is < 1 or > 9)
         {
             Console.WriteLine("The guess must be between 1-9. Try again");
             result = false;
